6459d8e5e8412e71400c4fd8e498556a42ceff95
Thomas Sungjin Kang <ujuc@ujuc.kr>
2020-01-13 10:50:40 +0900

    outstream 모듈 추가
    - stdio에서 쓰는 부분에 대해서 정의함.

---
+++ stdlib/outstream.py
@@ -0,0 +1,37 @@
+"""
+outstream.py
+
+``outstream`` 모듈은 OutStream 클래스를 정의한다.
+"""
+
+import sys
+
+
+class OutStream:
+    """
+    출력에 관련된 것들을 정의한다.
+    """
+    def __init__(self, f=None):
+        if f is None:
+            self._stream = sys.stdout
+        else:
+            self._stream = open(f, 'w', encoding='utf-8')
+
+    def writeln(self, x=''):
+        x = str(x)
+        self._stream.write(x)
+        self._stream.write('\n')
+        self._stream.flush()
+
+    def write(self, x=''):
+        x = str(x)
+        self._stream.write(x)
+        self._stream.flush()
+
+    def writef(self, fmt, *args):
+        x = fmt % args
+        self._stream.write(x)
+        self._stream.flush()
+
+    def __del__(self):
+        self._stream.close()
