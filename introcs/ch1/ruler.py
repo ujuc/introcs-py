102d4e3a8aaedcdb48571688e253809b10b2f03e
Thomas Sungjin Kang <ujuc@ujuc.kr>
2020-02-11 23:39:27 +0900

    feat: Add Ch1.2.1 Example

---
+++ introcs/ch1/ruler.py
@@ -0,0 +1,11 @@
+from introcs.stdlib import stdio
+
+ruler1 = '1'
+ruler2 = ruler1 + ' 2 ' + ruler1
+ruler3 = ruler2 + ' 3 ' + ruler2
+ruler4 = ruler3 + ' 4 ' + ruler3
+
+stdio.writeln(ruler1)
+stdio.writeln(ruler2)
+stdio.writeln(ruler3)
+stdio.writeln(ruler4)
